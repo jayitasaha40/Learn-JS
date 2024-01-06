8459c68dd6a9b60430df4014d8ffd852b231cfc2
jayitasaha40 <jayitasaha40@gmail.com>
2024-01-06 05:51:38 +0530

    Completed: Smoothness added

--- DrumKit/script.js
+++ DrumKit/script.js
@@ -7,5 +7,13 @@ function keyEvent(event) {
     var x = document.getElementById(id);
     x.play(); 
   } 
+
+  function pauseAudio(id) { 
+    var x = document.getElementById(id); //x = audio
+    x.pause(); 
+  } 
  // document.getElementById("body").addEventListener("keypress", keyEvent(Event));
-  
+ document.addEventListener('keyup', (event) => {
+    let key = event.key;
+    pauseAudio(key.toUpperCase());
+ });

884a36ee189842b739291a4b52e95165bde1d269
jayitasaha40 <jayitasaha40@gmail.com>
2024-01-09 01:42:03 +0530

    Style Added

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,12 +1,25 @@
+var user;
 document.getElementById('show-content-btn').addEventListener('click', function () {
+     user = 1;
     const initialContent = document.getElementById('initial-content');
     initialContent.style.display = 'none';
 
     const fullContent = document.getElementById('full-content');
     fullContent.style.display = 'block';
 
-    document.body.style.backgroundColor = 'white';
+    document.body.style.backgroundColor = ' #29272b;';
     setTimeout(function () {
         fullContent.style.opacity = '1';
     }, 10);
 });
+
+function changeValue(e){
+    if(user==1){
+        document.getElementById(e).innerHTML = "X";
+        user = 2;
+    }
+    else{
+        document.getElementById(e).innerHTML = "O";
+        user = 1;
+    }
+}

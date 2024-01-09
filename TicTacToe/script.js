0e870987835d45b267b3bbc9a35522f88aaf30a0
jayitasaha40 <jayitasaha40@gmail.com>
2024-01-10 00:28:38 +0530

    Sounds Added

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -7,6 +7,8 @@ document.getElementById('show-content-btn').addEventListener('click', function (
     const fullContent = document.getElementById('full-content');
     fullContent.style.display = 'block';
 
+    document.getElementById("player").innerHTML="Player 1";
+
     document.body.style.backgroundColor = ' #29272b;';
     setTimeout(function () {
         fullContent.style.opacity = '1';
@@ -14,12 +16,15 @@ document.getElementById('show-content-btn').addEventListener('click', function (
 });
 
 function changeValue(e){
+    document.getElementById("tap").play();
     if(user==1){
         document.getElementById(e).innerHTML = "X";
         user = 2;
+        document.getElementById("player").innerHTML="Player 2";
     }
     else{
         document.getElementById(e).innerHTML = "O";
         user = 1;
+        document.getElementById("player").innerHTML="Player 1";
     }
 }

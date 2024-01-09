eaa68fec15724bd1830aca7a6b66191edb510147
jayitasaha40 <jayitasaha40@gmail.com>
2024-01-10 01:15:46 +0530

    Style added

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -6,8 +6,9 @@ document.getElementById('show-content-btn').addEventListener('click', function (
 
     const fullContent = document.getElementById('full-content');
     fullContent.style.display = 'block';
+    setTimeout(display("Player 1"), 1000);
 
-    document.getElementById("player").innerHTML="Player 1";
+    
 
     document.body.style.backgroundColor = ' #29272b;';
     setTimeout(function () {
@@ -15,11 +16,15 @@ document.getElementById('show-content-btn').addEventListener('click', function (
     }, 10);
 });
 
+function display(text){
+    document.getElementById("player").innerHTML= text;
+}
 function changeValue(e){
     document.getElementById("tap").play();
     if(user==1){
         document.getElementById(e).innerHTML = "X";
         user = 2;
+        
         document.getElementById("player").innerHTML="Player 2";
     }
     else{

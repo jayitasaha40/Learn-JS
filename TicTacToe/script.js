3036378befd23822e5a1ff5322476f1ceef34469
jayitasaha40 <jayitasaha40@gmail.com>
2024-01-13 23:18:22 +0530

    Added Winning Pattern

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,4 +1,8 @@
 var user;
+const winningPattern = [[0,1,2],
+                        [3,4,5],
+                        [6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+                        
 document.getElementById('show-content-btn').addEventListener('click', function () {
      user = 1;
     const initialContent = document.getElementById('initial-content');

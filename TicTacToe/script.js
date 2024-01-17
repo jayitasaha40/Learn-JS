120c90700526571c806af1957584971863a3e596
jayitasaha40 <jayitasaha40@gmail.com>
2024-01-17 22:24:10 +0530

    Sound Effect logic added

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -26,22 +26,29 @@ function display(text){
     document.getElementById("player").innerHTML= text;
 }
 function changeValue(e){
-    document.getElementById("tap").play();
-    if(user==1){
+    
+    if(user==1 && document.getElementById(e).innerHTML!="X" && document.getElementById(e).innerHTML!="O"){
+        document.getElementById("tap").play();
         document.getElementById(e).innerHTML = "X";
         user = 2;
         
         document.getElementById("player").innerHTML="Player 2";
     }
-    else{
+    else if(user==2 && document.getElementById(e).innerHTML!="X" && document.getElementById(e).innerHTML!="O"){
+        document.getElementById("tap").play();
         document.getElementById(e).innerHTML = "O";
         user = 1;
         document.getElementById("player").innerHTML="Player 1";
     }
+    else{
+        document.getElementById("error").play();
+    }
     
-boxes.forEach(box => {
-    
-    console.log(box.textContent);
-});
+  winningPattern.forEach(pat => {
+    console.log( boxes[pat[0]].textContent);
+    if(boxes[pat[0]].textContent ==  boxes[pat[1]].textContent == boxes[pat[2]].textContent == 'X'){
+        console.log("JJJ")
+    }
+  });
 }
 //comment12

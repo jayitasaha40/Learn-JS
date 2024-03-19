65f51c81ed202e401df344e19b9038be52843418
jayitasaha40 <jayitasaha40@gmail.com>
2024-03-19 23:47:41 +0530

    Backend added

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -10,12 +10,29 @@ async function fetchData() {
 
     try {
         const response = await fetch(url, options);
-        const result = await response.text();
+        const result = await response.json();
         console.log(result);
+        document.getElementById("feels_like").innerText = " :- "+result['feels_like']
+        document.getElementById("cloud_pct").innerText = " :- "+result['cloud_pct']
+        document.getElementById("Sunrise").innerText = " :- "+result['sunrise']
+        document.getElementById("humidity").innerText = " :- "+result['humidity']
+        document.getElementById("max_temp").innerText = " :- "+result['max_temp']
+        document.getElementById("min_temp").innerText = " :- "+result['min_temp']
+        document.getElementById("Sunset").innerText = " :- "+result['sunset']
+        document.getElementById("temp").innerText = " :- "+result['temp']
+        document.getElementById("wind_degrees").innerText = " :- "+result['wind_degrees']
+        document.getElementById("wind_speed").innerText = " :- "+result['wind_speed']
+        return result;
+        
     } catch (error) {
         console.error(error);
     }
 }
 
 // Call the asynchronous function
-fetchData();
+let result = fetchData();
+
+
+
+
+

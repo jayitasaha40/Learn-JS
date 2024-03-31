55d58c1433599aeecad3869765ad9088d07f486e
jayitasaha40 <jayitasaha40@gmail.com>
2024-03-31 23:35:42 +0530

    Body added

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -1,7 +1,7 @@
 const board = document.getElementById("board");
-let bodyArr = {x:1,y:2};
+var bodyArr = [[3,5],[4,5]];
 // console.log(bodyArr.x);
-let dir = {x:0,y:0};
+
 
 //Show Food
 const food = document.createElement("div");
@@ -17,3 +17,53 @@ head.style.gridColumnStart = 5;
 head.style.gridRowStart = 2;
 board.append(head);
 
+//Showing Body
+for(var i = 0; i < bodyArr.length; i++) {
+    const body = document.createElement("div");
+    body.classList.add("snakeBody");
+    body.style.gridColumnStart = bodyArr[i][1];
+    body.style.gridRowStart = bodyArr[i][0];
+
+    const point = document.createElement("div");
+    point.classList.add("point");
+    body.append(point);
+    board.append(body);    
+}
+
+document.addEventListener("keydown", (e) =>{
+    let dir = {x:0,y:0}; //Start
+    let key = e.key;
+    
+    switch(key) {
+        case "ArrowRight":
+          // code block
+          dir.x = 1;
+          dir.y = 0;
+          break;
+        case "ArrowLeft":
+          // code block
+          dir.x = -1;
+          dir.y = 0;
+          break;
+        
+        case "ArrowDown":
+          // code block
+          dir.x = 0;
+          dir.y = 1;
+          break;
+        
+        case "ArrowUp":
+          // code block
+          dir.x = 0;
+          dir.y = -1;
+          break;
+        default:
+            break
+          // code block
+      }
+    
+    //   console.log(key);
+    //   console.log(dir);
+    
+    
+})
